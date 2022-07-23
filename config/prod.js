bfc86e2a7c421ca4816c7f463eb4a5e7e15ad5c0
sumeet32112 <sumeetsingh32112@gmail.com>
2022-07-23 12:44:01 +0530

    key changed

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports={
-    MONGOURI:process.env.MOGOURI,
-    JWT_SECRET:process.env.JWT_SEC
+    MONGOURI:'mongodb://showdown:OpPTGgZiBZXE5gDz@cluster0-shard-00-00.3bkwz.mongodb.net:27017,cluster0-shard-00-01.3bkwz.mongodb.net:27017,cluster0-shard-00-02.3bkwz.mongodb.net:27017/?ssl=true&replicaSet=atlas-pih5lj-shard-0&authSource=admin&retryWrites=true&w=majority',
+    JWT_SECRET:'dfjhkh@434738^!&a'
 }
